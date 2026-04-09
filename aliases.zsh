f17f4d57dd23863ce2c684672c88de1bc0af0060
donatomartin <uo288787@uniovi.es>
2026-04-09 19:48:30 +0200

    feat: add aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,16 @@
 ## Aliases
+
+###system
+alias c="clear"
+
+###vim
+alias nv="nvim";
+alias v="nvim";
+alias vim="nvim";
+
+###tmux
+alias t="tmux"
+
 ###wsl
 alias winget="winget.exe"
 alias pnpm="pnpm.exe"
@@ -8,8 +20,7 @@ alias docker="docker.exe"
 ###git
 alias ga="git add"
 alias gA="git add -A"
-alias gs="git status"
-alias gcm="git commit -m"
+alias gs="git status" alias gcm="git commit -m"
 alias gc="git commit"
 alias gd="git diff"
 alias gds="git diff --staged"
@@ -17,12 +28,20 @@ alias gdh="git diff HEAD"
 alias gl="git log --oneline --graph --decorate --all --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(auto)%h%d %s %C(brightblack)(%an, %ad) [%cd]'"
 alias gpsh="git push"
 
-###other
-alias vim="nvim"
-alias nf="fastfetch"
+###list
 alias l="eza --group-directories-first"
 alias ll="eza -l --group-directories-first"
 alias lo="eza --oneline --group-directories-first"
 alias lt="eza --tree"
 alias la="eza -a"
 alias lla="eza -la --group-directories-first"
+
+###nix
+alias ns="sudo nixos-rebuild switch --flake ~/nix-config"
+alias nt="sudo nixos-rebuild test --flake ~/nix-config"
+alias ngc="sudo nix-collect-garbage -d"
+alias ndg="sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations old && nix-env --delete-generations old"
+
+###other
+alias neofetch="fastfetch"
+
